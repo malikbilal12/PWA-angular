046fd0efc3d2b6ba055e99591806aec2bfc198ee
bilal hassan <mc180406267@gmail.com>
2024-06-20 16:30:30 +0500

    new app compete in pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   products: any[] = [];
+  cart: any[] = [];
   private apiUrl = 'http://localhost:3000/products';
+  private cartUrl = 'http://localhost:3000/cart';
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.fetchData();
+    this.fetchCart();
   }
 
   fetchData() {
     this.http.get<any[]>(this.apiUrl).subscribe(data => {
       this.products = data;
     });
+  }
+
+  fetchCart() {
+    this.http.get<any[]>(this.cartUrl).subscribe(data => {
+      this.cart = data;
+    });
+  }
+
+  addCart(product: any) {
+    if (product.stock > 0) {
+      product.stock -= 1;
+      this.updateProduct(product);
+      this.updateCart(product);
+    } else {
+      console.log('Product is out of stock');
+    }
+  }
+
+  updateProduct(product: any) {
+    const url = `${this.apiUrl}/${product.id}`;
+    this.http.put(url, product).subscribe(updatedProduct => {
+      console.log('Product updated:', updatedProduct);
+      this.fetchData();
+    }, error => {
+      console.error('Error updating product:', error);
+    });
+  }
+
+  updateCart(product: any) {
+    const cartProduct = this.cart.find(p => p.id === product.id);
+    if (cartProduct) {
+      cartProduct.quantity += 1;
+      this.http.put(`${this.cartUrl}/${cartProduct.id}`, cartProduct).subscribe(updatedCart => {
+        console.log('Cart updated:', updatedCart);
+        this.fetchCart();
+      }, error => {
+        console.error('Error updating cart:', error);
+      });
+    } else {
+      const newCartProduct = { ...product, quantity: 1 };
+      this.http.post(this.cartUrl, newCartProduct).subscribe(addedCart => {
+        console.log('Product added to cart:', addedCart);
+        this.fetchCart();
+      }, error => {
+        console.error('Error adding to cart:', error);
+      });
+    }
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  @Input() cart: any
 }
